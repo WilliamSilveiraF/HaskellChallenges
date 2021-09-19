44aa4cd73c59a04eb8fde4e0174d14751f085da3
williamsilveiraf <williamsilveiraf@hotmail.com>
2021-09-19 12:53:36 -0300

    circulo & polionomio

---
+++ circulo.hs
@@ -0,0 +1,5 @@
+area :: Float -> Float
+area raio = raio*raio*pi
+
+perimetro :: Float -> Float -> Float
+perimetro comp raio = 2*comp*raio
---
+++ polinomio.hs
@@ -0,0 +1,15 @@
+--primeiro programa em haskell
+polinomio :: Int -> Int
+polinomio x = x*x + 10*x + 2
+
+--calcula o quadrado de um inteiro
+quadrado :: Int -> Int
+quadrado n = n*n
+
+-- soma 
+soma :: Float -> Float -> Float
+soma a b = a + b
+
+--triplica
+triplica :: Int -> Int
+triplica var = var*3
