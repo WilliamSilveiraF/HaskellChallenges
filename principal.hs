545dd38d0cbd7b8116ff968581c711782a863197
williamsilveiraf <williamsilveiraf@hotmail.com>
2021-09-25 17:22:44 -0300

    UpdateAplicaPrimDose

--- principal.hs
+++ principal.hs
@@ -4,7 +4,7 @@ type CadastroSUS = [Cidadao]
 dataBankSUS::CadastroSUS
 dataBankSUS = 
     [(26716347665, "Paulo Souza", 'M', (11,10,1996),"Rua A, 202","Muribeca", "SE", "999997000", "psouza@gmail.com"),
-    (87717347115, "Ana Reis",'F', (5,4,1970), "Rua B, 304","Aracaju", "SE", "999826004", "areis@gmail.com"),
+    (3, "Ana Reis",'F', (5,4,1970), "Rua B, 304","Aracaju", "SE", "999826004", "areis@gmail.com"),
     (99999999999, "Guilherme Alves", 'M', (02,07,2002),"Rua C, 405","Salgado", "SE", "999997044", "guilherme@gmail.com"),
     (88888888888, "Esmeralda Oliveira", 'F', (09,09,2003),"Rua D, 506","Lagarto", "SE", "999996025", "esmeralda@gmail.com"),
     (10101010101, "Fernanda Menezes", 'F', (01,04,2000),"Rua E, 506","Lagarto", "SE", "999996025", "esmeralda@gmail.com")]
@@ -157,7 +157,9 @@ type Vacinado = (CPF, Doses)
 
 aplicaPrimDose :: CPF -> CadastroSUS -> FaixaIdade -> Municipio -> Vacina -> Data -> Vacinados -> Vacinados
 aplicaPrimDose citizenCPF dataBankSUS faixadeidade municipio vacina dataVacinacao dataBankVacinados
-   | length dataBankVacinados /= 0    = error "Você já tomou a primeira doze"
+   | (firstDoseApplied citizenCPF dataBankVacinados == True)    = error "Primeira dose já aplicada"  --Se minha primeira dose foi aplicada, retorne "Primeira dose já aplicada"
+   | (checaCPF citizenCPF dataBankSUS == False)                 = error "CPF não encontrado no Banco de Dados SUS"
+
 
 -- True == Minha primeira dose foi aplicada // False == Minha segunda dose não foi aplicada
 firstDoseApplied :: CPF -> Vacinados -> Bool
@@ -170,3 +172,5 @@ firstDoseApplied citizenCPF dataBankVacinados =
 
 getCPFVacinado :: Vacinado -> CPF
 getCPFVacinado (cpfVacinado,  _)  = cpfVacinado
+
+--checaIntervaloDeIdades :: 
